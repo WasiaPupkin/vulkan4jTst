9702018c134248280ae51e857c8552ae63973828
wasia <la5t-demonius@yandex.ru>
2025-04-07 23:40:18 +0300

    initial

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            var app = new Application();
+            app.run();
+        }
+        catch (Throwable e) {
+            e.printStackTrace(System.err);
+        }
+    }
+}
